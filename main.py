45af5b62fa9be31800de5c4f4ac1bb4577e1dfa4
Jaechul Jung <jaechul@jeongjaecheols-MacBook-Air.local>
2025-09-04 11:20:18 +0900

    Added optional 3rd argument --verbose that displays # of tokens used

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 import sys
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
 def main():
     load_dotenv()
@@ -11,19 +12,28 @@ def main():
     if len(sys.argv) < 2:
         print("I need a prompt!")
         sys.exit(1)
+    verbose_flag = False
+    if len(sys.argv) == 3 and sys.argv[2] == "--verbose":
+        verbose_flag = True
     # print("Args: ", sys.argv) - Returns ['main.py'] when no args are passed (uv run main.py)
     prompt = sys.argv[1]
 
+    messages = [
+       types.Content(role="user", parts=[types.Part(text=prompt)]),
+    ]
+
     response = client.models.generate_content(
         model="gemini-2.0-flash-001",
-        contents=prompt,
+        contents=messages,
     )
 
     print(response.text)
     if response is None or response.usage_metadata is None:
         print("No usage metadata found")
         return
-    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    if verbose_flag:
+        print(f"User prompt: {prompt}")
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
 
 main()
