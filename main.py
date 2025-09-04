388a0b05305ce9a1f5ed35737e366c7e36532893
Jaechul Jung <jaechul@jeongjaecheols-MacBook-Air.local>
2025-09-04 21:51:10 +0900

    Updated system prompt & Allow LLM to call CRUD functions

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ from dotenv import load_dotenv
 from google import genai
 from google.genai import types
 from functions.get_files_info import schema_get_files_info
+from functions.get_file_content import schema_get_file_content
+from functions.write_file import schema_write_file
+from functions.run_python_file import schema_run_python_file
 
 def main():
     load_dotenv()
@@ -16,6 +19,9 @@ def main():
     When a user asks a question or makes a request, make a function call plan. You can perform the following operations:
 
     - List files and directories
+    - Read the content of a file
+    - Write to a file (create of update)
+    - Run a Python file with optional arguments
 
     All paths you provide should be relative to the working directory. You do not need to specify the working directory in your function calls as it is automatically injected for security reasons.
     """
@@ -34,9 +40,12 @@ def main():
     ]
 
     available_functions = types.Tool(
-    function_declarations=[
-        schema_get_files_info,
-    ]
+        function_declarations=[
+            schema_get_files_info,
+            schema_get_file_content,
+            schema_write_file,
+            schema_run_python_file,
+        ]
     )
 
     config=types.GenerateContentConfig(
