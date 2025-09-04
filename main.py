f3f49afcf3cfafc78fd440dbb19a9f392702ee5d
Jaechul Jung <jaechul@jeongjaecheols-MacBook-Air.local>
2025-09-05 00:13:56 +0900

    Created the Agentic Loop

--- main.py
+++ main.py
@@ -53,25 +53,35 @@ def main():
     tools=[available_functions], system_instruction=system_prompt
     )
 
-    response = client.models.generate_content(
-        model="gemini-2.0-flash-001",
-        contents=messages,
-        config=config,
-    )
+    max_iters = 20
+    for i in range(0, max_iters):
+        response = client.models.generate_content(
+            model="gemini-2.0-flash-001",
+            contents=messages,
+            config=config,
+        )
 
-    if response is None or response.usage_metadata is None:
-        print("No usage metadata found")
-        return
-    if verbose_flag:
-        print(f"User prompt: {prompt}")
-        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+        if response is None or response.usage_metadata is None:
+            print("No usage metadata found")
+            return
+        if verbose_flag:
+            print(f"User prompt: {prompt}")
+            print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+            print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
 
-    if response.function_calls:
-        for function_call_part in response.function_calls:
-            result = call_function(function_call_part, verbose=verbose_flag)
-            print(result)
-    else:
-        print(response.text)
+        if response.candidates:
+            for candidate in response.candidates:
+                if candidate is None or candidate.content is None:
+                    continue
+                messages.append(candidate.content)
+        
+        if response.function_calls:
+            for function_call_part in response.function_calls:
+                result = call_function(function_call_part, verbose=verbose_flag)
+                messages.append(result)
+        else:
+            # final agent text message
+            print(response.text)
+            return
 
 main()
